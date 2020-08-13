a750b2263dd2c2c74a7834a708ee2d655d6c9045
BreeMadison <breannamadison711@gmail.com>
2020-08-12 20:49:12 -0400

    Formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
   return (
     <div className="App">
       <a href="http://localhost:8888/">
-      <button>Login With Spotify</button> 
+        <button>Login With Spotify</button>
       </a>
-      
     </div>
   );
 }
